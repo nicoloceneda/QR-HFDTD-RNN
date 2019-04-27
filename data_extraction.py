0fa3c5d38728ed2de40299aa6253ec66dc0346ff
Nicolo Ceneda <47401951+nicoloceneda@users.noreply.github.com>
2019-04-27 15:43:21 +0200

    Version 1
    Departure from the basic script

--- data_extraction.py
+++ data_extraction.py
@@ -18,23 +18,14 @@ import argparse
 # DATA EXTRACTION
 
 
-# SQL query function to extract the data
+# Function to run the SQL query and obviate occasional failures:
 
-def sql_query(table, symbol, nrows):
+def query_sql(table, symbol, start_time, end_time):
 
-    if nrows > 0:
-        query = "SELECT * FROM {} WHERE sym_root = '{}' LIMIT {}".format(table, symbol, nrows)
-    else:
-        query = "SELECT * FROM {} WHERE sym_root = '{}'".format(table, symbol)
+    max_attempts = 2
+    query = "SELECT * FROM {} WHERE sym_root = '{}' and time_m>= '{}' and time_m<= '{}'".format(table, symbol, start_time, end_time)        # TODO: Check how to handle multiple symbols
 
-    return query
-
-
-# Query attempt function to obviate occasional failures of the raw_sql(query) command
-
-def query_attempt(query, max_attempts):                                                                                                     # TODO: WRDS must be imported before calling
-
-    db = wrds.Connection()
+    dbi = wrds.Connection()
 
     for attempt in range(max_attempts):
 
@@ -46,33 +37,34 @@ def query_attempt(query, max_attempts):                                         
             else:
                 print('The query failed and the max number of attempts has been reached.')
         else:
-            db.close()
+            dbi.close()
             return data, True
 
-    db.close()
+    dbi.close()
     return None, False
 
 
-# Parameters
+# Class to call all the parameters for a selected table (i.e. date) and symbol(s)
 
-class TradeCleanParams:
+class TradesCleanedParameters:
 
     def __init__(self, arg):
-        self.start_time = arg.first_hour
-        self.end_time = arg.last_hour
-        self.col_types = {'ex': str, 'price': float, 'size': float}
+        self.start_time = arg.start_time
+        self.end_time = arg.end_time
+        self.col_types = {'ex': str, 'price': float, 'size': float}                                                                         # TODO: Add the time column
 
+
+# Class to clean the trades
 
 class TradesCleaner:
 
-    def __init__(self, data):
+    def __init__(self, data): # TODO: data_type <- 'trades' (string), cleaning_params (dictionary)
 
         self.data = data
-        self.data_type = 'trades'
-        self.args = TradeCleanParams(args)
+        self.args = TradesCleanedParameters(args)
 
-        if not set(self.args.col_types.keys()).issubset(self.data.columns):
-            raise ValueError('One or more columns are not contained in the queried data.')
+        if not set(self.args.col_types.keys()).issubset(self.data.columns): # TODO: if not {'ex', 'price', 'size'} is subset of
+            raise ValueError('Either "ex", "price" or "size" is not a column in the queried data.')
 
         self.make_time_idx()
         self.data = self.data.between_time(self.args.start_time, self.args.end_time)
@@ -81,8 +73,8 @@ class TradesCleaner:
 
     def make_time_idx(self):
 
-        datetime = self.data['date'].astype(str)+' '+self.data['time_m'].astype(str)
-        self.data = self.data.set_index(pd.DatetimeIndex(pd.to_datetime(datetime)))
+        datetime = self.data['date'].astype(str)+' '+self.data['time_m'].astype(str) # TODO: 2017-09-18 04:00:00.017518
+        self.data = self.data.set_index(pd.DatetimeIndex(datetime)) # TODO: DatetimeIndex <--  dtype: object
 
     def clean_trades(self):
 
@@ -93,69 +85,66 @@ class TradesCleaner:
 
 # Command-line interface to extract data
 
-if __name__ == '__main__':
+parser = argparse.ArgumentParser(description='Command-line interface to extract the data')
 
-    # Parser
+parser.add_argument('-sy', '--symbols', type=str, default='AAPL', nargs='+', help='Symbol(s) to process.')
+parser.add_argument('-sd', '--start_date', type=str, default='2003-09-10', help='Start date to extract the data.')                                 # TODO: check default dates
+parser.add_argument('-ed', '--end_date', type=str, default='2018-04-20', help='End date to extract the data.')
+parser.add_argument('-st', '--start_time', type=str, default='9:30', help='Start time to extract the data.')                                       # TODO: check the default values
+parser.add_argument('-et', '--end_time', type=str, default='15:30', help='End time to extract the data.')
+parser.add_argument('-bg', '--debug', action='store_true', help='Debuggin flag')
 
-    parser = argparse.ArgumentParser(description='something')
+args = parser.parse_args()
 
-    def check_nonnegative(value):
-        ivalue = int(value)
-        if ivalue < 0:
-            raise argparse.ArgumentTypeError('Error: enter an integer greater or equal to zero')
-        return ivalue
+# Debugging
 
-    parser.add_argument('--symbols', type=str, default='AAPL', nargs='+', help='Symbol(s) to process.')
-    parser.add_argument('--nrows', type=check_nonnegative, default=100, help='Number of rows to extract. If 0 then it loads all rows.')
-    parser.add_argument('--max_attempts', type=int, default=3, help='Number of attempts to try to query the database.')
-    parser.add_argument('--start_time', type=str, default='9:30', help='Start time to extract the data.')                                   # TODO: check the default values
-    parser.add_argument('--end_time', type=str, default='15:30', help='End time to extract the data.')
-    parser.add_argument('--start_date', type=str, default='2015-11-01', help='Start date to extract the data.')                             # TODO: check dates
-    parser.add_argument('--end_date', type=str, default='2017-01-01', help='End date to extract the data.')
+if args.debug:
+    args.start_date = '2018-09-17'
+    args.end_date = '2018-09-17'
+    args.start_time = '12:30:00'
+    args.end_time = '12:31:00'
 
-    args = parser.parse_args()
+# List of all tables in 'taqmsec'
 
-    # List of all tables in 'taqmsec'
+try:
+    import wrds
+except ImportError:
+    raise ImportError('An Error occurred trying to run the script locally. Run this script on the wrds cloud.')
+else:
+    db = wrds.Connection()
+    taqmsec_tables = db.list_tables(library='taqmsec')
+    db.close()
 
-    try:
-        import wrds
-    except ImportError:
-        raise ImportError('An Error occurred trying to run the script locally. Run this script on the wrds cloud.')
-    else:
-        db = wrds.Connection()
-        taqmsec_tables = db.list_tables(library='taqmsec')
-        db.close()
+# List of dates
 
-    # List of dates
+from pandas.tseries.holiday import USFederalHolidayCalendar
+from pandas.tseries.offsets import CustomBusinessDay
 
-    from pandas.tseries.holiday import USFederalHolidayCalendar
-    from pandas.tseries.offsets import CustomBusinessDay
+us_businessday = CustomBusinessDay(calendar=USFederalHolidayCalendar())
+dates_idx = pd.DatetimeIndex(start=args.start_date, end=args.end_date, freq=us_businessday)
+dates_list = [str(d)[0:10].replace('-', '') for d in dates_idx]
 
-    us_businessday = CustomBusinessDay(calendar=USFederalHolidayCalendar())
-    dates_idx = pd.DatetimeIndex(start=args.start_date, end=args.end_date, freq=us_businessday)
-    dates_list = [str(d)[0:10].replace('-', '') for d in dates_idx]
+# List of symbols
 
-    # List of symbols
+symbols_list = args.symbols
 
-    symbols_list = args.symbols
+# Extract data
 
-    # Extract data
+for sym in symbols_list:
 
-    for sym in symbols_list:
+    for date in dates_list:
 
-        for date in dates_list:
+        if 'ctm_'+date in taqmsec_tables:
 
-            if 'ctm_'+date in taqmsec_tables:
+            print('Running a query with: symbol = {}, date = {}, nrows={}.'.format(sym, date, args.nrows))
+            query_trades = query_sql('taqmsec.ctm_'+date, sym, args.nrows) # TODO: query_trades = "SELECT * FROM {taqmsec.ctm_ + 20170918} WHERE sym_root = '{}' LIMIT {} "
+            queried_trades, success_query_trades = query_attempt(query_trades, args.max_attempts) # TODO: queried_trades = db.raw_sql(query_trades)
 
-                print('Running a query with: symbol = {}, date = {}, nrows={}.'.format(sym, date, args.nrows))
-                query_trades = sql_query('taqmsec.ctm_'+date, sym, args.nrows)
-                queried_trades, success_query_trades = query_attempt(query_trades, args.max_attempts)
-
-                if success_query_trades:
-                    print('Cleaning the data.')
-                    trades_cleaner = TradesCleaner(queried_trades)
-                else:
-                    print('Could not load the data for security {} and date {}: skipping to the next date.'.format(sym, date))
-
+            if success_query_trades:
+                print('Cleaning the data.')
+                trades_cleaner = TradesCleaner()
             else:
-                print('Could not find the table in database.')
+                print('Could not load the data for security {} and date {}: skipping to the next date.'.format(sym, date))
+
+        else:
+            print('Could not find the table in database.')

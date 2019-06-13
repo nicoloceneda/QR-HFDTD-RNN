b7f4b45b2051696517174a178028b82a2fefd2bd
Nicolo Ceneda <47401951+nicoloceneda@users.noreply.github.com>
2019-06-14 00:13:16 +0200

    Updated shall for symbol list exec

--- extract_data_sl.sh
+++ extract_data_sl.sh
@@ -3,6 +3,6 @@
 #$ -m abe
 #$ -M nicolo.ceneda@student.unisg.ch
 source ~/virtualenv/bin/activate
-python3 extract_data.py -sl AAPL TSLA -sd 2019-03-28 -ed 2019-03-28 -st 09:30:00 -et 16:00:00 -po -go
+python3 extract_data.py -sl AAPL AMZN TSLA -sd 2019-03-28 -ed 2019-03-29 -st 09:35:00 -et 15:50:00 -po -go
 
 

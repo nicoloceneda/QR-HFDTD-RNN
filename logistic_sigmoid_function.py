52681241f184f2513a755a595236712bb32937eb
Nicolo Ceneda <47401951+nicoloceneda@users.noreply.github.com>
2020-02-26 17:42:53 +0100

    Add files via upload

---
+++ logistic_sigmoid_function.py
@@ -0,0 +1,80 @@
+""" LOGISTIC SIGMOID FUNCTION
+    -------------------------
+    Graph of the logistic sigmoid function
+"""
+
+
+# -------------------------------------------------------------------------------
+# 0. IMPORT LIBRARIES
+# -------------------------------------------------------------------------------
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# -------------------------------------------------------------------------------
+# 1. PLOT THE LOGISTIC SIGMOID FUNCTION
+# -------------------------------------------------------------------------------
+
+
+# Define the logistic sigmoid function
+
+def sigmoid(z):
+
+    return 1.0 / (1.0 + np.exp(-z))
+
+
+# Plot the logistic sigmoid function
+
+z = np.arange(-10, 10, 0.01)
+phi_z = sigmoid(z)
+
+fig, ax = plt.subplots()
+ax.plot(z, phi_z, color="blue")
+ax.axhline(0.0, linestyle='--', linewidth=0.5, color='k')
+ax.axhline(0.5, linestyle='--', linewidth=0.5, color='k')
+ax.axhline(1.0, linestyle='--', linewidth=0.5, color='k')
+ax.axvline(0.0, linestyle='--', linewidth=0.5, color='k')
+ax.set_xlabel("Z")
+ax.set_ylabel(r"$\phi(Z)$", fontsize=11)
+ax.set_title("Logistic Sigmoid Function")
+fig.tight_layout()
+plt.savefig('images/z_logistic_sigmoid_function.png')
+
+
+# Plot the logistic sigmoid function and the cost function
+
+
+
+
+def cost_1(z):
+
+    return - np.log(sigmoid(z))
+
+
+def cost_0(z):
+
+    return - np.log(1 - sigmoid(z))
+
+
+z = np.arange(-10, 10, 0.01)
+
+phi_z = sigmoid(z)
+c1 = cost_1(z)
+c0 = cost_0(z)
+
+fig, ax = plt.subplots()
+ax.plot(phi_z, c1, color="blue", label="J(w) if y=1")
+ax.plot(phi_z, c0, color="red", label="J(w) if y=0")
+ax.set_xlabel(r"$\phi(Z)$", fontsize=11)
+ax.set_ylabel("J(w)")
+ax.set_title("Logistic Sigmoid Function")
+ax.legend(loc="upper center")
+ax.set_xlim([0, 1])
+ax.set_ylim([0, 5.1])
+
+fig.tight_layout()
+plt.savefig('z_logistic_and_cost_functions.png')
+
+

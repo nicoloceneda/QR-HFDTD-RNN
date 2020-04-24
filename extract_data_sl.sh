cb567cae2a9d8271148d0dbf6f482aa96ebfc811
Nicolo Ceneda <47401951+nicoloceneda@users.noreply.github.com>
2020-04-24 23:51:26 +0200

    Add files via upload

--- extract_data_sl.sh
+++ extract_data_sl.sh
@@ -3,5 +3,5 @@
 #$ -m abe
 #$ -M nicolo.ceneda@student.unisg.ch
 source venv/bin/activate
-python3 extract_data.py -sl AAPL -sd 2019-03-04 -ed 2019-03-29 -st 09:35:00 -et 15:55:00
+python3 extract_data.py -sl AAPL -sd 2019-03-04 -ed 2019-03-08 -st 09:35:00 -et 15:55:00
 

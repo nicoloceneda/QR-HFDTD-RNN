7264dfd9c46f12d7825402c8af1ac675a07e3bd5
Nicolo Ceneda <47401951+nicoloceneda@users.noreply.github.com>
2020-04-30 00:41:31 +0200

    Add files via upload

--- lstm_rnn.py
+++ lstm_rnn.py
@@ -35,95 +35,22 @@ tf.random.set_seed(1)
 # -------------------------------------------------------------------------------
 
 
-""" FOR DEBUGGING PT 1
-    symbol = 'AAPL'
-    elle = 200
-    X = pd.read_csv('datasets/mode sl/datasets split/AAPL/X.csv')
-    Y = pd.read_csv('datasets/mode sl/datasets split/AAPL/Y.csv')
-"""
-
-
-# Import the extracted datasets
+# Import the train, validation and test sets
 
 symbol = 'AAPL'
-data_extracted = pd.read_csv('datasets/mode sl/datasets/' + symbol + '/data.csv')
 
+X_train = pd.read_csv('datasets/mode sl/datasets std/' + symbol + '/X_train.csv')
+X_valid = pd.read_csv('datasets/mode sl/datasets std/' + symbol + '/X_valid.csv')
+X_test = pd.read_csv('datasets/mode sl/datasets std/' + symbol + '/X_test.csv')
 
-# Create the features and target datasets
-
-log_return = np.diff(np.log(data_extracted['price']))
-data = pd.DataFrame({'log_return': log_return})
-
-elle = 200
-data['log_return_ma'] = data['log_return'].rolling(window=elle).mean()
-
-X = []
-Y = []
-
-pbar = pyprind.ProgBar(50000)
-
-for pos in range(elle, data.shape[0]):
-
-    Y.append(data.iloc[pos]['log_return'])
-
-    data_past = pd.DataFrame(data.iloc[pos - elle: pos], copy=True)
-    r_past = data_past['log_return']
-    r_past_ma = data_past.iloc[-1]['log_return_ma']
-    r_diff = r_past - r_past_ma
-    data_past['log_return_d2'] = r_diff ** 2
-    data_past['log_return_d3'] = r_diff ** 3
-    data_past['log_return_d4'] = r_diff ** 4
-    X.append(data_past[['log_return', 'log_return_d2', 'log_return_d3', 'log_return_d4']])
-
-    pbar.update()
-
-X = pd.concat(X, ignore_index=True)
-Y = pd.DataFrame(Y, columns=['label'])
-
-
-""" FOR DEBUGGING PT 2
-    X.to_csv('datasets/mode sl/datasets split/AAPL/X.csv', index=False)
-    Y.to_csv('datasets/mode sl/datasets split/AAPL/Y.csv', index=False)
-"""
-
-
-# Define the training, validation and test subsets
-
-train_split = int(Y.shape[0] * 0.8)
-valid_split = train_split + int(Y.shape[0] * 0.1)
-test_split = train_split + int(Y.shape[0] * 0.1) + int(Y.shape[0] * 0.1)
-
-X_train = pd.DataFrame(X.iloc[:train_split * elle], copy=True)
-Y_train = pd.DataFrame(Y.iloc[:train_split], copy=True)
-
-X_valid = pd.DataFrame(X.iloc[train_split * elle: valid_split * elle], copy=True)
-Y_valid = pd.DataFrame(Y.iloc[train_split: valid_split], copy=True)
-
-X_test = pd.DataFrame(X.iloc[valid_split * elle: test_split * elle], copy=True)
-Y_test = pd.DataFrame(Y.iloc[valid_split: test_split], copy=True)
-
-
-# Standardize the training, validation and test subsets
-
-for column in X_train.columns:
-
-    column_mean = X_train[column].mean()
-    column_std = X_train[column].std()
-
-    X_train[column] = (X_train[column] - column_mean) / column_std
-    X_valid[column] = (X_valid[column] - column_mean) / column_std
-    X_test[column] = (X_test[column] - column_mean) / column_std
-
-Y_mean = Y_train.mean()
-Y_std = Y_train.std()
-
-Y_train = (Y_train - Y_mean) / Y_std
-Y_valid = (Y_valid - Y_mean) / Y_std
-Y_test = (Y_test - Y_mean) / Y_std
+Y_train = pd.read_csv('datasets/mode sl/datasets std/' + symbol + '/Y_train.csv')
+Y_valid = pd.read_csv('datasets/mode sl/datasets std/' + symbol + '/Y_valid.csv')
+Y_test = pd.read_csv('datasets/mode sl/datasets std/' + symbol + '/Y_test.csv')
 
 
 # Reshape the train, validation and test subsets
 
+elle = 200
 n_features = 4
 
 X_train = X_train.values.reshape(-1, elle, n_features)

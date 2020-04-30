f3daa3536782d068078274c5854edd679bef878a
Nicolo Ceneda <47401951+nicoloceneda@users.noreply.github.com>
2020-04-30 18:10:54 +0200

    Add files via upload

--- benchmark_garch.m
+++ benchmark_garch.m
@@ -18,7 +18,7 @@ Y_test = table2array(readtable("NASDAQ100/data_100/Y_test.csv"));
 
 % Hyperparameter selection
 
-losses = zeros(4,4);
+losses = zeros(4, 4);
 minimum_loss = 1e8;
 
 for p = 1:4
@@ -33,11 +33,11 @@ for p = 1:4
         
         tau = [0.01, 0.05: 0.05: 0.95, 0.99];                            
         Q = qn_calculator(valid_sigma_garch, tau);                        
-        losses(p,q) = pinball_loss_function(Y_valid, Q, tau);           
+        losses(p, q) = pinball_loss_function(Y_valid, Q, tau);           
         
-        if losses(p,q) < minimum_loss
+        if losses(p, q) < minimum_loss
             
-            minimum_loss = losses(p,q);
+            minimum_loss = losses(p, q);
             best_p = p;
             best_q = q;
             
@@ -84,7 +84,7 @@ disp([best_p, best_q, loss]);
 
 % Hyperparameter selection
 
-losses = zeros(4,4);
+losses = zeros(4, 4);
 minimum_loss = 1e8;
 
 for p = 1:4
@@ -100,11 +100,11 @@ for p = 1:4
         
         tau = [0.01, 0.05: 0.05: 0.95, 0.99];                            
         Q = qt_calculator(valid_sigma_garch, tau, nu);                        
-        losses(p,q) = pinball_loss_function(Y_valid, Q, tau);           
+        losses(p, q) = pinball_loss_function(Y_valid, Q, tau);           
         
-        if losses(p,q) < minimum_loss
+        if losses(p, q) < minimum_loss
             
-            minimum_loss = losses(p,q);
+            minimum_loss = losses(p, q);
             best_p = p;
             best_q = q;
             
@@ -150,6 +150,74 @@ disp([best_p, best_q, loss]);
 % -------------------------------------------------------------------------------
 
 
+% Hyperparameter selection
+
+losses = zeros(4, 4, 4);
+minimum_loss = 1e8;
+
+for p = 1:4
+    
+    for q = 1:4
+        
+        for r = 1:4
+        
+            model_garch = arima('ARLags', r, 'Variance', garch('GARCHLags', p, 'ARCHLags', q, 'Distribution', 't'));
+            estimated_garch = estimate(model_garch, Y_train, 'Display', 'off');
+            [residual_garch, cond_variance_garch] = infer(estimated_garch, [Y_train; Y_valid]);
+            valid_mean_garch = Y_valid - residual_garch(length(Y_train) + 1: length(Y_train) + length(Y_valid));
+            valid_variance_garch = cond_variance_garch(length(Y_train) + 1: length(Y_train) + length(Y_valid));
+            nu = estimated_garch.Variance.Distribution.DoF;
+            valid_sigma_garch = sqrt(valid_variance_garch * (nu - 2) / nu);                  
+        
+            tau = [0.01, 0.05: 0.05: 0.95, 0.99];                            
+            Q = qtar_calculator(valid_sigma_garch, valid_mean_garch, tau, nu);                        
+            losses(p,q, r) = pinball_loss_function(Y_valid, Q, tau);           
+        
+            if losses(p, q, r) < minimum_loss
+            
+                minimum_loss = losses(p, q, r);
+                best_p = p;
+                best_q = q;
+                best_r = r;
+            
+            end
+        
+        end
+        
+    end
+    
+end
+
+
+% Calculate the volatility for the test set
+
+model_garch = arima('ARLags', best_r, 'Variance', garch('GARCHLags', best_p, 'ARCHLags', best_q, 'Distribution', 't'));
+estimated_garch = estimate(model_garch, Y_train, 'Display', 'off');
+[residual_garch, cond_variance_garch] = infer(estimated_garch, [Y_train; Y_valid; Y_test]);
+test_mean_garch = Y_test - residual_garch(length(Y_train) + length(Y_valid) + 1: length(Y_train) + length(Y_valid) + length(Y_test));
+test_variance_garch = cond_variance_garch(length(Y_train) + length(Y_valid) + 1: length(Y_train) + length(Y_valid) + length(Y_test));
+nu = estimated_garch.Variance.Distribution.DoF;
+test_sigma_garch = sqrt(test_variance_garch * (nu - 2) / nu); 
+
+
+% Calculate the loss for the tau set
+
+tau = [0.01, 0.05: 0.05: 0.95, 0.99];
+Q = qtar_calculator(test_sigma_garch, test_mean_garch, tau, nu);
+loss = pinball_loss_function(Y_test, Q, tau);
+
+disp("GARCH-t + AR: Test set params and loss for tau:");
+disp([best_p, best_q, best_r, loss]);
+
+
+% Calculate the loss for the new tau set
+
+tau = [0.01,0.05,0.1];
+Q = qtar_calculator(test_sigma_garch, test_mean_garch, tau, nu);
+loss = pinball_loss_function(Y_test, Q, tau);
+
+disp("GARCH-t + AR: Test set params and loss for new tau:");
+disp([best_p, best_q, best_r, loss]);
 
 
 % -------------------------------------------------------------------------------
@@ -159,20 +227,30 @@ disp([best_p, best_q, loss]);
 
 % Define a function to calculate Q for a normal distribution
 
-function Q = qn_calculator(Y_predicted, tau)
+function Q = qn_calculator(sigma, tau)
 
     z_tau = norminv(tau);
-    Q = Y_predicted * z_tau;
+    Q = sigma * z_tau;
     
 end
 
 
 % Define a function to calculate Q for a student distribution
 
-function Q = qt_calculator(Y_predicted, tau, nu)
+function Q = qt_calculator(sigma, tau, nu)
 
     z_tau = tinv(tau, nu);
-    Q = Y_predicted * z_tau;
+    Q = sigma * z_tau;
+    
+end
+
+
+% Define a function to calculate Q for a student distribution where returns are AR
+
+function Q = qtar_calculator(sigma, mean, tau, nu)
+
+    z_tau = tinv(tau, nu);
+    Q = mean + sigma * z_tau;
     
 end
 

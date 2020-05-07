f5361d338997eb69bd1561f1d892ecdce1e35019
Nicolo Ceneda <47401951+nicoloceneda@users.noreply.github.com>
2020-05-07 23:19:14 +0200

    Add files via upload

--- extract_data_sl.sh
+++ extract_data_sl.sh
@@ -3,5 +3,4 @@
 #$ -m abe
 #$ -M nicolo.ceneda@student.unisg.ch
 source venv/bin/activate
-python3 extract_data.py -sl AAPL -sd 2019-03-04 -ed 2019-03-08 -st 09:35:00 -et 15:55:00
-
+python3 extract_data.py -sl AAPL AMD AMZN CSCO FB INTC JPM MSFT NVDA TSLA -sd 2019-03-04 -ed 2019-07-19 -st 09:35:00 -et 15:55:00
